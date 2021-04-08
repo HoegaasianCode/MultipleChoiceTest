c359d9618d7c6c0ea54b7f9de9bc707add8ef5d2
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-04-08 23:27:10 +0200

    Update Program.cs

--- MultipleChoiceTestMaker/Program.cs
+++ MultipleChoiceTestMaker/Program.cs
@@ -1,25 +1,22 @@
-﻿using System;
+using System;
 
 namespace MultipleChoiceTestMaker
 {
     class Program
     {
-        /// <summary>
-        /// https://edabit.com/challenge/thXMEAWNWyk9cCZcM
-        /// JUST STARTED ;)
-        /// </summary>
+        // https://edabit.com/challenge/thXMEAWNWyk9cCZcM
+        // Solved - although this assignment could do with tests
 
-        static void Main(string[] args) //  "Maths: Passed! (80%)" 
+        static void Main(string[] args)
         {
-            TestPaper paper1 = new("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
-            TestPaper paper2 = new("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
-            TestPaper paper3 = new("Computing", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
-            Student student1 = new();
-            Student student2 = new();
-            student1.TakeTest(paper1, new string[] { "1A", "2D", "3D", "4A", "5A" });
-            student1.CountCorrectAnswers();
-            student1.CalcPercentage();
-
+            Student student = new();
+            TestPaper paper = new("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
+            TestPaper paper1 = new("Algebra", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
+            student.TakeTest(paper, new string[] { "1C", "2D", "3A", "4C" });
+            student.TakeTest(paper1, new string[] { "1A", "2C", "3A", "4C", "5D", "6C", "7B" });
+            student.ListToSortedArray();
+            string[] results = student.TestsTaken;
+            foreach (string result in results) Console.WriteLine(result);
         }
     }
 }
