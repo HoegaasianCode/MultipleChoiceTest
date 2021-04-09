a97ed3ab40c1e5e2fc441a25e81d2127b044c0a3
HoegaasianCode <80755360+HoegaasianCode@users.noreply.github.com>
2021-04-09 08:25:53 +0200

    Update Student.cs

--- MultipleChoiceTestMaker/Student.cs
+++ MultipleChoiceTestMaker/Student.cs
@@ -7,8 +7,6 @@ namespace MultipleChoiceTestMaker
     public class Student : IStudent
     {
         public string[] TestsTaken { get; set; }
-        private TestPaper _testPaper;
-        private string[] _answers;
         private readonly List<string> ResultList;
 
         public Student()
@@ -19,32 +17,30 @@ namespace MultipleChoiceTestMaker
 
         public void TakeTest(TestPaper paper, string[] answers)
         {
-            _testPaper = paper;
-            _answers = answers;
-            CalcPercentage();
+            CalcPercentage(paper, answers);
         }
 
-        private void CalcPercentage()
+        private void CalcPercentage(TestPaper testPaper, string[] answers)
         {
             int j = 0;
-            for(int i = 0; i < _answers.Length; i++)
+            for(int i = 0; i < answers.Length; i++)
             {
-                string answer = _answers[i];
-                string correctAnwer = _testPaper.MarkScheme[i];
+                string answer = answers[i];
+                string correctAnwer = testPaper.MarkScheme[i];
                 if (answer == correctAnwer) j++;
             }
-            double percentage = Math.Round((double)j / _testPaper.MarkScheme.Length * 100, 0);
-            BuildString(percentage);
+            double percentage = Math.Round((double)j / testPaper.MarkScheme.Length * 100, 0);
+            BuildString(percentage, testPaper);
         }
 
-        private void BuildString(double percentage)
+        private void BuildString(double percentage, TestPaper testPaper)
         {
             string result;
             string passedText = "Not Passed!";
             string percentText = percentage.ToString() + '%';
-            int passMark = Convert.ToInt32(_testPaper.PassMark.Remove(_testPaper.PassMark.Length - 1));
+            int passMark = Convert.ToInt32(testPaper.PassMark.Remove(testPaper.PassMark.Length - 1));
             if (percentage > passMark) passedText = "Passed!";
-            result = _testPaper.Subject + ':' + ' ' + passedText + ' ' + percentText;
+            result = testPaper.Subject + ':' + ' ' + passedText + ' ' + percentText;
             ResultList.Add(result);
         }
 
